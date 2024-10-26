b362d7209beb61baa0424bf49fc22059cededa8b
Tejas <tejaspawar2026@gmail.com>
2024-10-26 21:22:24 +0530

    g-pages router issue update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import "./App.css";
 import RoutesConfig from "./routes";
 import AppNavbar from "./components/layout/navbar/AppNavbar";
